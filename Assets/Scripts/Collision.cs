35a4873da1fe2ce707cf49ead1bae54928eda819
Just_Abe <barahm230@morrisville.edu>
2022-02-12 19:01:08 -0500

    Simple Follow Camera

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    void OnCollisionEnter(Collision other) 
+    void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Ouch!!");
     }
 
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
      {
          Debug.Log("What was that?!?!");
         
