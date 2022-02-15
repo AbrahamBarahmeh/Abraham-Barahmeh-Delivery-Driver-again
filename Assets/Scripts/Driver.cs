7d77d5f164593b3615ad6078ec76bfcb4b2a6b0f
Just_Abe <barahm230@morrisville.edu>
2022-02-15 15:52:03 -0500

    Boosts & Bumps

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-     [SerializeField]float steerSpeed = 0.1f;
-     [SerializeField]float moveSpeed = 0.01f;
-    void Start()
-    {
-        
-    }
-
+     [SerializeField] float steerSpeed = 0.1f;
+     [SerializeField] float moveSpeed = 7f;
+     [SerializeField] float slowSpeed = 6f;
+     [SerializeField] float boostSpeed = 20f;
+   
     void Update()
     {
      float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
      float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
      transform.Rotate(0,0,-steerAmount);   
      transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Boost")
+        {
+         moveSpeed = boostSpeed;
+        }
     }
 }
